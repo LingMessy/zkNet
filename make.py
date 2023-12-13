1c56a74d2022edafbc4f8b859173636f69602d66
lingmessy <lingmessy@163.com>
2023-12-13 19:34:34 +0800

    改进项目构建工具，完全使用python构建，移除make工具依赖

--- make.py
+++ make.py
@@ -1,41 +1,120 @@
+#!/usr/bin/env python3
+
 import os
 import platform as pf
+import sys
 
 version = "0.9.1"
 
-platform = pf.system().lower()
-arch = pf.architecture()[0]
-machine = pf.machine()
+PYTHON = "python"
+REQU = " ".join(("requests", "pyinstaller"))
+
+platform = (pf.system().lower(), pf.architecture()[0], pf.machine())
+
+
+def buildWin64():
+    os.system(
+        "call wvenv/Scripts/activate.bat && pyinstaller -F -w gui.py -n zkNet-v%s-x64-win.exe"
+        % version
+    )
+
+
+def buildWin32():
+    os.system(
+        "call wvenv/Scripts/activate.bat && pyinstaller -F -w gui.py -n zkNet-v%s-x86-win.exe"
+        % version
+    )
+
+
+def buildLinux64():
+    os.system(
+        "source venv/bin/activate && pyinstaller -F -w gui.py -n zkNet-v%s-x64-linux.bin"
+        % version
+    )
+
+
+def buildLinux32():
+    os.system(
+        "source venv/bin/activate && pyinstaller -F -w gui.py -n zkNet-v%s-x86-linux.bin"
+        % version
+    )
+
+
+def runWin():
+    os.system("call wvenv/Scripts/activate.bat && python gui.py")
+
+
+def runLinux():
+    os.system("source venv/bin/activate && python gui.py")
+
+
+def venvWin():
+    os.system(
+        f"{PYTHON} -m venv wvenv && call wvenv/Scripts/activate.bat && pip install {REQU}"
+    )
+
+
+def venvLinux():
+    os.system(
+        f"{PYTHON} -m venv venv && source venv/bin/activate && pip install {REQU}"
+    )
+
+
+def cleanWin():
+    os.system("rmdir /s /Q build dist __pycache__ venv wvenv & del /Q *.spec")
+
+
+def cleanLinux():
+    os.system("rm -rf build dist *.spec __pycache__ venv wvenv")
+
+
+# TODO 有待补充，测试过的平台可以加到这里
+supportPlatformTable = [
+    # system    arch    machine     build    run    venv        clean
+    ("windows", "64bit", "AMD64", buildWin64, runWin, venvWin, cleanWin),
+    ("linux", "64bit", "x86_64", buildLinux64, runLinux, venvLinux, cleanLinux),
+]
+
+
+def build():
+    for i in supportPlatformTable:
+        if i[:3] == platform:
+            i[3]()
+            print("当前平台为：%s-%s-%s" % platform)
+            return
+    print("当前平台为：%s-%s-%s" % platform)
+    print("该平台未被支持")
+
+
+argvTable = [("build", 3), ("run", 4), ("venv", 5), ("clean", 6)]
 
 if __name__ == "__main__":
-    if platform == "windows":
-        # TODO 有待补充，测试过的平台可以加到这里
-        winX64Machine = ["AMD64"]
-        winX86Machine = []
+    funcIdx = None
 
-        if arch == "64bit" and machine in winX64Machine:
-            os.system("pyinstaller -F -w gui.py -n zkNet-v%s-x64-win.exe" % version)
+    if len(sys.argv) != 2:
+        print("useage: python make.py <option>")
+        sys.exit(0)
 
-        elif arch == "32bit" and machine in winX86Machine:
-            os.system("pyinstaller -F -w gui.py -n zkNet-v%s-x86-win.exe" % version)
+    for arg, fIdx in argvTable:
+        if sys.argv[1] == arg:
+            funcIdx = fIdx
+            break
 
-        else:
-            print("该平台未被支持")
+    if funcIdx is None:
+        print("option not support")
+        sys.exit(0)
 
-    elif platform == "linux":
-        # TODO 有待补充，测试过的平台可以加到这里
-        linuxX64Machine = ["x86_64"]
-        linuxX86Machine = []
-        if arch == "64bit" and machine == "x86_64":
-            os.system("pyinstaller -F -w gui.py -n zkNet-v%s-x64-linux.bin" % version)
+    platformIdx = None
+    for i, v in enumerate(supportPlatformTable):
+        if v[:3] == platform:
+            platformIdx = i
+            break
 
-        elif arch == "32bit" and machine == "i386":
-            os.system("pyinstaller -F -w gui.py -n zkNet-v%s-x86-linux.bin" % version)
-
-        else:
-            print("该平台未被支持")
-
-    else:
+    if platformIdx is None:
+        print("platform not support")
         print("该平台未被支持")
+        sys.exit(0)
 
-    print("当前平台为：%s-%s-%s" % (platform, arch, machine))
+    supportPlatformTable[platformIdx][funcIdx]()
+
+    print("当前平台为：%s-%s-%s" % platform)
